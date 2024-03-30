57243abf8f63755c613f0fe1523c8e3fd2589101
MadeshiAshlesha <ashleshaprivate@gmail.com>
2024-03-30 19:57:38 +0530

    change in form

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,18 +1,18 @@
 import express from "express";
 import {
-    addRemoveFriend,
-    getUser,
-    getUserFriends
+  getUser,
+  getUserFriends,
+  addRemoveFriend,
 } from "../controllers/users.js";
 import { verifyToken } from "../middleware/auth.js";
 
-const router= express.Router();
+const router = express.Router();
 
 /* READ */
 router.get("/:id", verifyToken, getUser);
-router.get(":id/friends", verifyToken, getUserFriends);
+router.get("/:id/friends", verifyToken, getUserFriends);
 
 /* UPDATE */
-router.patch("/:id/:friendsId", verifyToken, addRemoveFriend);
+router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
 
 export default router;
